68fb8923d51edca42086f1172ad1cdc20e5cc77e
Mcnoor <99060667+Mcnoor@users.noreply.github.com>
2022-07-14 21:23:51 -0500

    test

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Comment } = require('../../models');
+const { Comment } = require('../../models/indexs');
 const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
